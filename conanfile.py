f3ec33fbdcb1f6bc8eace7bbc9f3200a82b7eabc
Brian Szmyd <bszmyd@ebay.com>
2018-09-05 18:07:23 -0600

    v2.0.5 - stable options

--- conanfile.py
+++ conanfile.py
@@ -2,7 +2,7 @@ from conans import ConanFile, CMake, tools
 
 class IOMgrConan(ConanFile):
     name = "iomgr"
-    version = "2.0.4"
+    version = "2.0.5"
     license = "Proprietary"
     description = "iomgr"
 
@@ -10,7 +10,7 @@ class IOMgrConan(ConanFile):
     options = {"shared": [True, False], "fPIC": [True]}
 
     requires = (("libevent/2.0.22@bincrafters/stable"),
-                ("sds_logging/3.2.1@sds/stable"))
+                ("sds_logging/3.2.2@sds/stable"))
 
     generators = "cmake"
     default_options = "shared=False", "fPIC=True"
