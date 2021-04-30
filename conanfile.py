290d71fcd15c9541c08dcb4199de811ae7ef2b6a
hkadayam <hkadayam@ebay.com>
2021-04-29 19:12:24 -0700

    Disabling test_msg spdk temporarily till we fix the hang issue (#135)

--- conanfile.py
+++ conanfile.py
@@ -4,7 +4,7 @@ from conans import ConanFile, CMake, tools
 
 class IOMgrConan(ConanFile):
     name = "iomgr"
-    version = "4.1.26"
+    version = "4.1.27"
 
     revision_mode = "scm"
     license = "Proprietary"
