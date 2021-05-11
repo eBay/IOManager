f10305536b530805f211bfc58d8b434116466eae
soungupta <soungupta@ebay.com>
2021-05-11 01:27:18 -0700

    updated the get_version api

--- src/lib/iomgr.cpp
+++ src/lib/iomgr.cpp
@@ -322,9 +322,7 @@ void IOManager::stop() {
     if (m_is_spdk) { stop_spdk(); }
 }
 
-std::string IOManager::get_version() const {
-    return PACKAGE_VERSION;
-}
+const std::string& IOManager::get_version() const { return PACKAGE_VERSION; }
 
 void IOManager::stop_spdk() {
     spdk_thread_lib_fini();
