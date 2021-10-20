75fef558791768986d7f64a1902a54b101b28aa9
hkadayam <hkadayam@ebay.com>
2021-10-20 16:45:26 -0700

    SDSTOR-5740 Start subsystem init instead of only bdev to work with spdk 21.07 (#182)

--- src/lib/iomgr.cpp
+++ src/lib/iomgr.cpp
@@ -14,6 +14,7 @@ extern "C" {
 #include <spdk/thread.h>
 #include <spdk/bdev.h>
 #include <spdk/env_dpdk.h>
+#include <spdk/init.h>
 #include <rte_errno.h>
 #include <rte_mempool.h>
 #include <rte_malloc.h>
@@ -123,10 +124,10 @@ void IOManager::start(size_t const num_threads, bool is_spdk, const thread_state
     m_rand_worker_distribution = std::uniform_int_distribution< size_t >(0, m_worker_reactors.size() - 1);
 
     if (is_spdk && init_bdev) {
-        LOGINFO("Initializing bdev subsystem");
+        LOGINFO("Initializing all spdk subsystems");
         iomanager.run_on(thread_regex::least_busy_worker, [this](io_thread_addr_t taddr) {
-            spdk_bdev_initialize(
-                [](void* cb_arg, int rc) {
+            spdk_subsystem_init(
+                [](int rc, void* cb_arg) {
                     IOManager* pthis = (IOManager*)cb_arg;
                     pthis->mempool_metrics_populate();
                     pthis->set_state_and_notify(iomgr_state::running);
@@ -262,7 +263,7 @@ void IOManager::stop() {
 
     if (m_is_spdk) {
         iomanager.run_on(thread_regex::least_busy_worker, [this](io_thread_addr_t taddr) {
-            spdk_bdev_finish(
+            spdk_subsystem_fini(
                 [](void* cb_arg) {
                     IOManager* pthis = (IOManager*)cb_arg;
                     pthis->set_state_and_notify(iomgr_state::stopping);
