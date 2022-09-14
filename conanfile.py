53d7ceb69b97ab92dce6a128bc819df6db39edf1
Ravi Akella email = raakella@ebay.com <raakella@sdsbuild07>
2022-09-14 16:19:42 -0700

    bump minor version

--- conanfile.py
+++ conanfile.py
@@ -2,7 +2,7 @@ from conans import ConanFile, CMake, tools
 
 class IOMgrConan(ConanFile):
     name = "iomgr"
-    version = "8.5.7"
+    version = "8.6.0"
     homepage = "https://github.corp.ebay.com/SDS/iomgr"
     description = "Asynchronous event manager"
     topics = ("ebay", "nublox")
