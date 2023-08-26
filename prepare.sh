9d73227f9adaebbd84c01ca2dfbfc267c3a63742
Brian Szmyd <brian.szmyd@protonmail.com>
2023-08-26 14:55:58 -0600

    Add simple script to prepare custom recipes. (#48)

---
+++ prepare.sh
@@ -0,0 +1,17 @@
+set -eu
+
+echo -n "Installing ELF tools..."
+sudo apt-get install -y python3-pyelftools libaio-dev > /dev/null
+pipx install pyelftools > /dev/null
+echo "done."
+echo -n "Exporting custom recipes..."
+echo -n "dpdk."
+conan export 3rd_party/dpdk
+echo -n "fio."
+conan export 3rd_party/fio
+echo -n "gprc_internal."
+conan export 3rd_party/grpc_internal
+echo -n "spdk."
+conan export 3rd_party/spdk
+
+echo "done."
