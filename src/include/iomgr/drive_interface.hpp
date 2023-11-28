009c4378a4d42c5f3a07e14ec14f9b090ca5c684
Brian Szmyd <bszmyd@ebay.com>
2023-11-27 18:23:38 -0700

    Workaround for Debug only.

--- src/include/iomgr/drive_interface.hpp
+++ src/include/iomgr/drive_interface.hpp
@@ -24,7 +24,13 @@
 #include <system_error>
 
 #include <nlohmann/json.hpp>
+#ifndef NDEBUG
+#define __kernel_timespec linux_timespec
+#endif
 #include <folly/futures/Future.h>
+#ifndef NDEBUG
+#undef __kernel_timespec
+#endif
 #include <iomgr/io_interface.hpp>
 #include <iomgr/iomgr_types.hpp>
 #include <iomgr/fiber_lib.hpp>
