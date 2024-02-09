433dd1528c49c24854ef2a16c3f7a827ba067334
Brian Szmyd <bszmyd@ebay.com>
2024-02-09 09:57:05 -0700

    Need version for recipe export

--- prepare.sh
+++ prepare.sh
@@ -7,7 +7,7 @@ python -m pip install pyelftools > /dev/null
 echo "done."
 echo -n "Exporting custom recipes..."
 echo -n "dpdk."
-conan export 3rd_party/dpdk
+conan export 3rd_party/dpdk dpdk/21.05@
 echo -n "fio."
 conan export 3rd_party/fio
 echo -n "spdk."
