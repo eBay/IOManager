35d32e6ce7cb466cebde284509eb9e305e238a13
Brian Szmyd <bszmyd@ebay.com>
2024-02-09 10:34:21 -0700

    Lift fio recipe

--- 3rd_party/fio/conanfile.py
+++ 3rd_party/fio/conanfile.py
@@ -1,9 +1,9 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+from conan import ConanFile
+from conan.tools.gnu import Autotools, AutotoolsToolchain
+from conan.tools.files import get, copy
+from os.path import join
 
-from conans import ConanFile, AutoToolsBuildEnvironment
-from conans import tools
-import os
+required_conan_version = ">=1.60.0"
 
 class LibFIOConan(ConanFile):
     name = "fio"
@@ -17,34 +17,32 @@ class LibFIOConan(ConanFile):
     options = {
         "native_build": [True, False],
         }
-    default_options = (
-        "native_build=False",
-        )
+    default_options = {
+        "native_build": 'False',
+    }
     source_subfolder = "source_subfolder"
 
     def configure(self):
         del self.settings.compiler.libcxx
 
     def source(self):
-        tools.get("{0}/archive/fio-{1}.tar.gz".format(self.homepage, self.version))
-        os.rename("%s-%s-%s" % (self.name, self.name, self.version), self.source_subfolder)
-#        tools.patch(strip=0, base_path=self.source_subfolder, patch_file="arch.patch")
-#        tools.patch(strip=0, base_path=self.source_subfolder, patch_file="rm_raw.patch")
+        get(self, "{0}/archive/fio-{1}.tar.gz".format(self.homepage, self.version), strip_root=True)
+
+    def generate(self):
+        tc = AutotoolsToolchain(self)
+        tc.configure_args = []
+        if not self.options.native_build:
+            tc.configure_args.append("--disable-native")
+        tc.generate()
 
     def build(self):
-        autotools = AutoToolsBuildEnvironment(self)
-        cargs = []
-        if not self.options.native_build:
-            cargs.append("--disable-native")
-        env_vars = autotools.vars
-        with tools.environment_append(env_vars):
-            with tools.chdir(self.source_subfolder):
-                autotools.configure(args=cargs)
-                autotools.make()
+        autotools = Autotools(self)
+        autotools.configure()
+        autotools.make()
 
     def package(self):
-        self.copy("fio", dst="bin", src="{}/".format(self.source_subfolder), keep_path=False)
-        self.copy("*.h", dst="include/fio", src="{}/".format(self.source_subfolder), keep_path=True)
+        copy(self, "fio", self.build_folder, join(self.package_folder, "bin"), keep_path=False)
+        copy(self, "*.h", self.source_folder, dst=join(self.package_folder, "include/fio"), keep_path=True)
 
     def deploy(self):
         self.copy("fio", dst="/usr/local/bin/", src="bin")
