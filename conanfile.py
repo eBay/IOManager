25e188a3c038745325756306b854aa500e54c23d
Yaming Kuang <yaming.kuang@gmail.com>
2024-05-29 17:49:19 -0700

    hdd stream default to 64

--- conanfile.py
+++ conanfile.py
@@ -5,7 +5,7 @@ from conans import CMake
 
 class IOMgrConan(ConanFile):
     name = "iomgr"
-    version = "8.7.7"
+    version = "8.7.8"
     homepage = "https://github.com/eBay/IOManager"
     description = "Asynchronous event manager"
     topics = ("ebay", "nublox", "aio")
