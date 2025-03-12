9f01977f0ca6b572771e32c4647031ff41f044ce
raakella1 <114193113+raakella1@users.noreply.github.com>
2025-03-12 08:52:49 -0700

    update sisl (#105)
    Co-authored-by: Ravi Nagarjun Akella <raakella1@$HOSTNAME>

--- conanfile.py
+++ conanfile.py
@@ -5,7 +5,7 @@ from conans import CMake
 
 class IOMgrConan(ConanFile):
     name = "iomgr"
-    version = "8.8.1"
+    version = "8.8.2"
     homepage = "https://github.com/eBay/IOManager"
     description = "Asynchronous event manager"
     topics = ("ebay", "nublox", "aio")
